f66894846426fe43a680fb7757b0c79a08174b9d
muhsintags <115772257+muhsintags@users.noreply.github.com>
2023-12-16 22:02:05 +0400

    Sun

---
+++ DondurmeKodu.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DondurmeKodu : MonoBehaviour
+{
+    public float donmeHizi = 5f;
+
+    void Update()
+    {
+        // Etrafta dönme hareketini sağla
+        transform.Rotate(Vector3.up, donmeHizi * Time.deltaTime);
+    }
+}
